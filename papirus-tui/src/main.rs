112fad79b934b8f3485fea33c6536880120dfb55
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-11-18 18:28:12 +0530

    feat(tui): use custom theme

--- papirus-tui/src/main.rs
+++ papirus-tui/src/main.rs
@@ -1,15 +1,39 @@
 pub(crate) mod types;
 pub(crate) mod views;
 
-use crate::views::app::app_view;
+use cursive::style::BaseColor;
+use cursive::theme::{BorderStyle, Theme};
+use cursive::theme::{Color, Palette, PaletteColor};
 use cursive::traits::Resizable;
 use cursive::views::Dialog;
+
+use crate::views::app::app_view;
 
 fn main() {
     let mut siv = cursive::default();
 
+    siv.set_theme(default_theme());
     siv.set_window_title("pAPIrus");
     siv.add_fullscreen_layer(Dialog::around(app_view()).title("pAPIrus").full_screen());
 
     siv.run();
+}
+
+fn default_theme() -> Theme {
+    let mut palette = Palette::default();
+
+    palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::Shadow] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::View] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::Primary] = Color::Light(BaseColor::White);
+    palette[PaletteColor::Secondary] = Color::Dark(BaseColor::White);
+    palette[PaletteColor::Tertiary] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::Highlight] = Color::Light(BaseColor::Green);
+    palette[PaletteColor::HighlightInactive] = Color::Dark(BaseColor::Green);
+
+    Theme {
+        shadow: false,
+        borders: BorderStyle::Simple,
+        palette,
+    }
 }
