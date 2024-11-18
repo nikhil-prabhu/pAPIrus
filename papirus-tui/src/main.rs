d8da0e69f7fba2c01ef5994e289deb768e8b2a67
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-11-18 17:11:33 +0530

    feat(tui): add tui entrypoint

--- papirus-tui/src/main.rs
+++ papirus-tui/src/main.rs
@@ -1,3 +1,15 @@
+use cursive::views::{Dialog, TextView};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+
+    siv.set_window_title("pAPIrus");
+    siv.add_global_callback('q', |s| s.quit());
+    siv.add_fullscreen_layer(
+        Dialog::around(TextView::new("Hello World! (press 'q' to quit)"))
+            .title("pAPIrus")
+            .button("Quit", |s| s.quit()),
+    );
+
+    siv.run();
 }
